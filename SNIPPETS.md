7b0210b792559a7e2bea52d865f8e90ab6133c6c
Niklas Gray <niklas@ourmachinery.com>
2021-12-13 14:46:44 -0800

    Updated snippets and macros

--- SNIPPETS.md
+++ SNIPPETS.md
@@ -1,5 +1,13 @@
-SNIPPETS
+# Snippets
 
+~~~bash
 git stash && git pull --rebase && git push && git stash pop && tmbuild
+~~~
 
+https://www.techtronic.us/pbcopy-pbpaste-for-wsl/
 
+~~~bash
+hub pr checkout NNN
+git merge master
+git reset master --soft
+~~~
--- macros.cmd
+++ macros.cmd
@@ -4,3 +4,4 @@ DOSKEY om=cd c:\work\ourmachinery.com $T$T title ourmachinery.com
 DOSKEY tm=cd c:\work\themachinery $T$T title The Machinery
 DOSKEY debug=c:\work\themachinery\bin\debug\the-machinery.exe $*
 DOSKEY ls=dir /w
+DOSKEY vscmd="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
