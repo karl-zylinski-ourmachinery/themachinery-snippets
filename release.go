914ca800827a0b97b7f1a0946395e593817685f5
Niklas Gray <niklas@ourmachinery.com>
2022-01-27 11:46:54 -0800

    Updated release script for regular release

--- release.go
+++ release.go
@@ -1,3 +1,11 @@
+// Script for putting together a release build. Run it from the `gitignore` folder inside
+// `themachinery` folder like this:
+//
+//     go run release.go
+//
+// The script will guide you through the release process (note that some steps will need to be
+// performed manually).
+
 package main
 
 import (
@@ -5,7 +13,6 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
-	"log"
 	"os"
 	"os/exec"
 	"path"
@@ -76,8 +83,15 @@ func Run(cmd *exec.Cmd) {
 	cmd.Stdout = os.Stdout
 	err := cmd.Run()
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
+}
+
+// Tries to run the command, printing output and returns the error status.
+func TryRun(cmd *exec.Cmd) error {
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	return cmd.Run()
 }
 
 func ManualStep(s, details string) {
@@ -94,39 +108,45 @@ func CopyFileToDir(srcFile, dir string) {
 	dstFile := path.Join(dir, path.Base(srcFile))
 	src, err := os.Open(srcFile)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	defer src.Close()
 	dst, err := os.Create(dstFile)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	defer dst.Close()
 	_, err = io.Copy(dst, src)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 }
 
 func UploadFileToWebsiteDir(srcFile, dir, password string) {
 	c, err := ftp.Dial("160.153.16.15:21")
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
 	err = c.Login("ourmachinery", password)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
 	err = c.ChangeDir(dir)
 	if err != nil {
-		log.Fatal(err)
+		err = c.MakeDir(dir)
+		if err == nil {
+			err = c.ChangeDir(dir)
+		}
+		if err != nil {
+			panic(err)
+		}
 	}
 
 	f, err := os.Open(srcFile)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	base := path.Base(srcFile)
 	err = c.Stor(base, f)
@@ -143,7 +163,7 @@ func HotFixLink(version string) string {
 	return strings.ReplaceAll(version, ".", "")
 }
 
-func buildWindowsPackage() {
+func stepBuildWindowsPackage() {
 	const STEP_CLEAN = "Clean directory"
 	if !HasCompletedStep(STEP_CLEAN) {
 		Run(exec.Command("tmbuild", "--clean"))
@@ -166,7 +186,7 @@ func buildWindowsPackage() {
 	}
 }
 
-func uploadWindowsPackage(version string) {
+func stepUploadWindowsPackage(version string) {
 	windowsPackage := "build/the-machinery-" + version + "-windows.zip"
 	windowsPdbsPackage := "build/the-machinery-pdbs-" + version + "-windows.zip"
 
@@ -174,7 +194,7 @@ func uploadWindowsPackage(version string) {
 	if !HasCompletedStep(STEP_UPLOAD_WINDOWS_TO_DROPBOX) {
 		// TODO: Get Dropbox path from user settings somehow...
 		dropbox := "C:/Users/nikla/Dropbox (Our Machinery)/Our Machinery Everybody"
-		dir := path.Join(dropbox, "releases/early-adopter", Major(version))
+		dir := path.Join(dropbox, "releases/2022", Major(version))
 		CopyFileToDir(windowsPackage, dir)
 		CopyFileToDir(windowsPdbsPackage, dir)
 		CompleteStep(STEP_UPLOAD_WINDOWS_TO_DROPBOX)
@@ -189,40 +209,175 @@ func uploadWindowsPackage(version string) {
 	}
 }
 
-func hotfixRelease() {
-	version := ReadSetting("Hotfix version number (M.m.p)")
-
+func stepCreateReleaseBranch(version string) {
 	const STEP_CHECK_OUT_SOURCE = "Check out source code"
 	if !HasCompletedStep(STEP_CHECK_OUT_SOURCE) {
-		Run(exec.Command("git", "checkout", "release/"+Major(version)))
+		Run(exec.Command("git", "checkout", "-b", "release/"+Major(version)))
 		os.Chdir("../sample-projects")
-		Run(exec.Command("git", "checkout", "release-"+Major(version)))
+		Run(exec.Command("git", "pull"))
+		Run(exec.Command("git", "tag", "release-"+Major(version)))
+		Run(exec.Command("git", "push", "--tags"))
+		os.Chdir("../ourmachinery.com")
+		Run(exec.Command("git", "pull"))
 		os.Chdir("../themachinery")
 		CompleteStep(STEP_CHECK_OUT_SOURCE)
 	}
+}
 
+func stepUpdateVersionNumbers(version string) {
 	// TODO: Automate this step.
 	const STEP_UPDATE_VERSION_NUMBERS = "Update version numbers"
 	ManualStep(STEP_UPDATE_VERSION_NUMBERS, "Update version numbers in the_machinery.h and *-package.json.")
+}
 
-	buildWindowsPackage()
-	uploadWindowsPackage(version)
+func stepUpdateDebugBinaries() {
+	const STEP_UPDATE_DEBUG_BINARIES = "Update debug binaries"
+	if !HasCompletedStep(STEP_UPDATE_DEBUG_BINARIES) {
+		Run(exec.Command("tmbuild", "--clean"))
+		CompleteStep(STEP_UPDATE_DEBUG_BINARIES)
+	}
+}
 
+func stepBuildSampleProjects(version string) {
+	const STEP_BUILD_SAMPLE_PROJECTS = "Build sample projects"
+	if !HasCompletedStep(STEP_BUILD_SAMPLE_PROJECTS) {
+		Run(exec.Command("bin/Debug/the-machinery.exe", "--safe-mode", "-t", "task-export-projects"))
+		os.Chdir("../sample-projects")
+		Run(exec.Command("git", "commit", "-am", "Updated sample projects for release-"+Major(version)))
+		Run(exec.Command("git", "tag", "release-"+Major(version)))
+		Run(exec.Command("git", "push"))
+		Run(exec.Command("git", "push", "--tags"))
+		os.Chdir("../themachinery")
+		CompleteStep(STEP_BUILD_SAMPLE_PROJECTS)
+	}
+}
+
+func stepRebuildSampleProjects() {
+	const STEP_REBUILD_SAMPLE_PROJECTS = "Rebuild sample projects -- git should be clean"
+	if !HasCompletedStep(STEP_REBUILD_SAMPLE_PROJECTS) {
+		Run(exec.Command("bin/Debug/the-machinery.exe", "--safe-mode", "-t", "task-export-projects"))
+		os.Chdir("../sample-projects")
+		Run(exec.Command("git", "status"))
+		os.Chdir("../themachinery")
+		CompleteStep(STEP_REBUILD_SAMPLE_PROJECTS)
+	}
+}
+
+func stepUploadSampleProjects(version string) {
+	samples := []string{}
+	files, err := os.ReadDir("../sample-projects")
+	if err != nil {
+		panic(err)
+	}
+	for _, file := range files {
+		if strings.HasSuffix(file.Name(), ".7z") {
+			samples = append(samples, path.Join("../sample-projects", file.Name()))
+		}
+	}
+
+	const STEP_UPLOAD_SAMPLE_PROJECTS_TO_DROPBOX = "Upload Sample Projects to Dropbox"
+	if !HasCompletedStep(STEP_UPLOAD_SAMPLE_PROJECTS_TO_DROPBOX) {
+		// TODO: Get Dropbox path from user settings somehow...
+		dropbox := "C:/Users/nikla/Dropbox (Our Machinery)/Our Machinery Everybody"
+		dir := path.Join(dropbox, "releases/2022", Major(version))
+		os.Mkdir(dir, 0777)
+		for _, sample := range samples {
+			CopyFileToDir(sample, dir)
+		}
+		CompleteStep(STEP_UPLOAD_SAMPLE_PROJECTS_TO_DROPBOX)
+	}
+
+	const STEP_UPLOAD_SAMPLE_PROJECTS_TO_WEBSITE = "Upload Sample Projects to website"
+	if !HasCompletedStep(STEP_UPLOAD_SAMPLE_PROJECTS_TO_WEBSITE) {
+		password := ReadSetting("Website password")
+		dir := "public_html/releases/" + Major(version)
+		for _, sample := range samples {
+			UploadFileToWebsiteDir(sample, dir, password)
+		}
+		CompleteStep(STEP_UPLOAD_SAMPLE_PROJECTS_TO_WEBSITE)
+	}
+}
+
+func sampleProjectName(fileName string) string {
+	if strings.HasPrefix(fileName, "animation-") {
+		return "Animation"
+	} else if strings.HasPrefix(fileName, "creation-graphs") {
+		return "Creation Graphs"
+	} else if strings.HasPrefix(fileName, "gameplay-first-person-") {
+		return "Gameplay First Person"
+	} else if strings.HasPrefix(fileName, "gameplay-third-person-") {
+		return "Gameplay Third Person"
+	} else if strings.HasPrefix(fileName, "gameplay-interaction-system-") {
+		return "Gameplay Interaction System"
+	} else if strings.HasPrefix(fileName, "modular-dungeon-kit-") {
+		return "Modular Dungeon Kit"
+	} else if strings.HasPrefix(fileName, "physics-") {
+		return "Physics"
+	} else if strings.HasPrefix(fileName, "pong-") {
+		return "Pong"
+	} else if strings.HasPrefix(fileName, "ray-tracing-hello-triangle-") {
+		return "Ray Tracing: Hello Triangle"
+	} else if strings.HasPrefix(fileName, "sound-") {
+		return "Sound"
+	} else if strings.HasPrefix(fileName, "sample-projects-") {
+		return "All Sample Projects"
+	} else {
+		panic("Unknown sample project name: " + fileName)
+	}
+}
+
+func stepUpdateEngineSampleProjectLinks(version string) {
+	samples := make([]os.DirEntry, 0)
+	files, err := os.ReadDir("../sample-projects")
+	if err != nil {
+		panic(err)
+	}
+	for _, file := range files {
+		if strings.HasSuffix(file.Name(), ".7z") {
+			samples = append(samples, file)
+		}
+	}
+
+	const STEP_UPDATE_ENGINE_SAMPLE_PROJECT_LINKS = "Update engine sample project links"
+	if !HasCompletedStep(STEP_UPDATE_ENGINE_SAMPLE_PROJECT_LINKS) {
+		// TODO: Automate this step.
+		fmt.Println("Update the `sample_projects` table in `download_tab.c` with the following:")
+		fmt.Println()
+
+		var all string
+		fmt.Println("struct project sample_projects[] = {")
+		for _, sample := range samples {
+			info, err := sample.Info()
+			if err != nil {
+				panic(err)
+			}
+			s := fmt.Sprintf("    { %-30s %-81s %8d },\n", "\""+sampleProjectName(sample.Name())+"\",", "\"https://ourmachinery.com/releases/"+version+"/"+sample.Name()+"\",", info.Size())
+			if strings.HasPrefix(sample.Name(), "sample-projects") {
+				all = s
+			} else {
+				fmt.Print(s)
+			}
+		}
+		fmt.Print(all)
+		fmt.Println("};")
+
+		fmt.Println()
+		fmt.Println("Press <Enter> to continue when done...")
+		fmt.Scanln()
+		CompleteStep(STEP_UPDATE_ENGINE_SAMPLE_PROJECT_LINKS)
+	}
+}
+
+func stepCommitChanges(version string) {
 	const STEP_COMMIT_CHANGES = "Commit changes"
 	if !HasCompletedStep(STEP_COMMIT_CHANGES) {
 		Run(exec.Command("git", "commit", "-a", "-m", "Release "+version))
 		Run(exec.Command("git", "push"))
 		CompleteStep(STEP_COMMIT_CHANGES)
 	}
+}
 
-	ManualStep("Build on Linux", "Reboot to Linux and run the build script there.")
-
-	// TODO: Automate this step.
-	ManualStep("Update website links", "Update the links on the download page with the links to the new project.")
-
-	// TODO: Automate this step.
-	ManualStep("Add Release Notes", "Add Release Notes for the hot fix release.")
-
+func stepBuildWebsite() {
 	const STEP_VERIFY_WEBSITE = "Verify website"
 	if !HasCompletedStep(STEP_VERIFY_WEBSITE) {
 		os.Chdir("../ourmachinery.com")
@@ -231,7 +386,7 @@ func hotfixRelease() {
 		hugoServe.Stderr = os.Stderr
 		err := hugoServe.Start()
 		if err != nil {
-			log.Fatal(err)
+			panic(err)
 		}
 		Run(exec.Command("rundll32", "url.dll,FileProtocolHandler", "http://localhost:1313/"))
 		ManualStep(STEP_VERIFY_WEBSITE, "Verify that website is working")
@@ -266,13 +421,18 @@ func hotfixRelease() {
 		CompleteStep(UPLOAD_WEBSITE)
 	}
 
+}
+
+func stepPushTags(version string) {
 	const PUSH_TAGS = "Push tags"
 	if !HasCompletedStep(PUSH_TAGS) {
 		Run(exec.Command("git", "tag", "release-"+version))
 		Run(exec.Command("git", "push", "--tags"))
 		CompleteStep(PUSH_TAGS)
 	}
+}
 
+func stepMergeToMaster(version string) {
 	const MERGE_TO_MASTER = "Merge to master"
 	if !HasCompletedStep(MERGE_TO_MASTER) {
 		os.Chdir("../sample-projects")
@@ -284,20 +444,69 @@ func hotfixRelease() {
 		Run(exec.Command("git", "push"))
 		CompleteStep(MERGE_TO_MASTER)
 	}
+}
+
+func release() {
+	version := ReadSetting("Release version number (M.m)")
+	stepCreateReleaseBranch(version)
+	stepUpdateVersionNumbers(version)
+	stepUpdateDebugBinaries()
+	stepBuildSampleProjects(version)
+	stepRebuildSampleProjects()
+	stepUploadSampleProjects(version)
+	stepUpdateEngineSampleProjectLinks(version)
+	stepBuildWindowsPackage()
+	stepUploadWindowsPackage(version)
+
+	ManualStep("Build on Linux", "Reboot to Linux and run the build script there.")
+	ManualStep("Update website links", "Update the links on the download page with the links to the new project.")
+	ManualStep("Add Release Notes", "Add Release Notes for the hot fix release.")
+	ManualStep("Update website roadmap", "Update the roadmap on the website")
+
+	stepBuildWebsite()
+	stepPushTags(version)
+	stepMergeToMaster(version)
+}
+
+func hotfixRelease() {
+	version := ReadSetting("Hotfix version number (M.m.p)")
+
+	const STEP_CHECK_OUT_SOURCE = "Check out source code"
+	if !HasCompletedStep(STEP_CHECK_OUT_SOURCE) {
+		Run(exec.Command("git", "checkout", "release/"+Major(version)))
+		os.Chdir("../sample-projects")
+		Run(exec.Command("git", "checkout", "release-"+Major(version)))
+		os.Chdir("../themachinery")
+		CompleteStep(STEP_CHECK_OUT_SOURCE)
+	}
+
+	stepUpdateVersionNumbers(version)
+	stepBuildWindowsPackage()
+	stepUploadWindowsPackage(version)
+	stepCommitChanges(version)
+
+	ManualStep("Build on Linux", "Reboot to Linux and run the build script there.")
+
+	ManualStep("Update website links", "Update the links on the download page with the links to the new project.")
+	ManualStep("Add Release Notes", "Add Release Notes for the hot fix release.")
+
+	stepBuildWebsite()
+	stepPushTags(version)
+	stepMergeToMaster(version)
 
 	const UPDATE_DOWNLOADS_CONFIGS = "Update themachinery/the-machinery-downloads-configs.json"
 	if !HasCompletedStep(UPDATE_DOWNLOADS_CONFIGS) {
 		dropbox := "C:/Users/nikla/Dropbox (Our Machinery)/Our Machinery Everybody"
-		dir := path.Join(dropbox, "releases/early-adopter", Major(version))
+		dir := path.Join(dropbox, "releases/2022", Major(version))
 		windowsPackage := path.Join(dir, "the-machinery-"+version+"-windows.zip")
 		linuxPackage := path.Join(dir, "the-machinery-"+version+"-linux.zip")
 		windowsStat, err := os.Stat(windowsPackage)
 		if err != nil {
-			log.Fatal(err)
+			panic(err)
 		}
 		linuxStat, err := os.Stat(linuxPackage)
 		if err != nil {
-			log.Fatal(err)
+			panic(err)
 		}
 
 		s := `
@@ -412,7 +621,7 @@ func linuxBuildFromScratch() {
 
 	const STEP_UPLOAD_LINUX_TO_DROPBOX = "Upload Linux package to Dropbox"
 	if !HasCompletedStep(STEP_UPLOAD_LINUX_TO_DROPBOX) {
-		Run(exec.Command("/bin/sh", "-c", "firefox https://www.dropbox.com/work/Our%20Machinery%20Everybody/releases/early-adopter/2021.11"))
+		Run(exec.Command("/bin/sh", "-c", "firefox https://www.dropbox.com/work/Our%20Machinery%20Everybody/releases/2022/2021.11"))
 		ManualStep(STEP_UPLOAD_LINUX_TO_DROPBOX, "Upload "+linuxPackage+" and "+linuxSymbolsPackage+"to Dropbox")
 	}
 
@@ -427,6 +636,7 @@ func linuxBuildFromScratch() {
 
 func main() {
 	os.Chdir("..")
-	hotfixRelease()
+	release()
+	// hotfixRelease()
 	// linuxBuildFromScratch()
 }
